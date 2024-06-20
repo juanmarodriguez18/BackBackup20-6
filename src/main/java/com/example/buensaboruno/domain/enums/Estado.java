60b40f1d8fb96473c4bb553cbdb6b0225b542b5e
juanmarodriguez18 <juanm.rodriguez@alumnos.frm.utn.edu.ar>
2024-06-20 20:07:30 -0300

    subiendo repo para render

---
+++ src/main/java/com/example/buensaboruno/domain/enums/Estado.java
@@ -0,0 +1,14 @@
+package com.example.buensaboruno.domain.enums;
+
+public enum Estado {
+
+    PENDIENTE,
+    PREPARACION,
+    LISTO_PARA_ENTREGA,
+    EN_DELIVERY,
+    CANCELADO,
+    RECHAZADO,
+    ENTREGADO,
+    FACTURADO
+
+}
